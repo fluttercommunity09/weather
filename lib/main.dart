92a1908ab404cfeec89f5c56eabd6b33e8983d2b
Discord <Discord@192.168.1.13>
2025-09-24 21:32:37 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather App',
+      title: 'Weather App ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFF2196F3),
